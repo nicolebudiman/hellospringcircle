78e53aac1fb54984b741ac135a984152d4bfd27a
Nicole Budiman <nicole.budiman@thoughtworks.com>
2020-09-10 14:28:46 +0800

    initial commit

---
+++ src/main/java/com/example/hellospringcircle/HellospringcircleApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellospringcircle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellospringcircleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellospringcircleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hellospringcircle/HelloControllerTest.java
@@ -0,0 +1,4 @@
+package com.example.hellospringcircle;
+
+public class HelloControllerTest {
+}
